6dbf0d7d0afdb1e04c179f588e63cf4d381b70fb
Feope <feopegaming@gmail.com>
2024-01-10 01:43:29 -0500

    Add project files.

---
+++ Tar Crafter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tar_Crafter
+{
+    public class Class1
+    {
+    }
+}
